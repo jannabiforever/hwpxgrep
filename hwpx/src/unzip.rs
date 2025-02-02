e58ee9db89e4c1c26b9f380ec16f1913cc9efafa
jannabiforever <jungini1226@gmail.com>
2025-02-02 19:53:09 +0900

    removed image handling and impled todo

--- hwpx/src/unzip.rs
+++ hwpx/src/unzip.rs
@@ -1,15 +1,14 @@
 use anyhow::Result;
-use image::DynamicImage;
 use regex::Regex;
 use std::{
-    io::{Read, Seek},
+    fs::{self, File},
+    io::{Read, Seek, Write},
     path::Path,
 };
 use zip::read::ZipFile;
 
 lazy_static::lazy_static! {
     static ref XML_REG_EXP: Regex = Regex::new(r"Contents/section\d+\.xml").unwrap(); // No need to handle errors here.
-    static ref IMAGE_REG_EXP: Regex = Regex::new(r"BinData/image\d+\.(?i:jpg|bmp)").unwrap(); // No need to handle errors here.
 }
 
 /// Main container of hwpx file.
@@ -32,9 +31,9 @@ impl<R: Read + Seek> HwpxFile<R> {
     }
 }
 
-impl HwpxFile<std::fs::File> {
+impl HwpxFile<File> {
     pub fn from_file_path<P: AsRef<Path>>(path: P) -> Result<Self> {
-        Self::new(std::fs::File::open(path)?)
+        Self::new(File::open(path)?)
     }
 }
 
@@ -43,28 +42,28 @@ impl HwpxFile<std::fs::File> {
 #[derive(Debug)]
 pub struct Extracted {
     pub xmls: Vec<String>,
-    pub images: Vec<DynamicImage>,
 }
 
 impl Extracted {
     fn new() -> Self {
-        Self {
-            xmls: Vec::new(),
-            images: Vec::new(),
-        }
+        Self { xmls: Vec::new() }
     }
 
     fn push_content(&mut self, content: String) {
         self.xmls.push(content)
     }
-
-    fn push_image(&mut self, image: DynamicImage) {
-        self.images.push(image)
-    }
 }
 
-// TODO: Implement this
-pub fn save_extracted<P: AsRef<Path>>(_: &Extracted, _: P) -> anyhow::Result<()> {
+/// Save extracted datum in given path.
+pub fn save_extracted<P: AsRef<Path>>(data: &Extracted, path: P) -> anyhow::Result<()> {
+    let path = path.as_ref().join("xmls");
+    fs::create_dir_all(&path)?;
+
+    for (idx, xml) in data.xmls.iter().enumerate() {
+        let mut file = File::create(path.join(format!("{idx}.xml")))?;
+        file.write_all(xml.as_bytes())?;
+    }
+
     Ok(())
 }
 
@@ -72,9 +71,8 @@ pub fn save_extracted<P: AsRef<Path>>(_: &Extracted, _: P) -> anyhow::Result<()>
 fn try_get_file(file: &mut ZipFile, res: &mut Extracted) -> Result<()> {
     if is_content(file.name()) {
         res.push_content(get_content(file)?);
-    } else if is_image(file.name()) {
-        res.push_image(get_image(file)?);
     }
+
     Ok(())
 }
 
@@ -86,17 +84,5 @@ fn get_content(file: &mut ZipFile) -> Result<String> {
     let mut content = String::new();
     file.read_to_string(&mut content)?;
     Ok(content)
-}
-
-fn is_image(name: &str) -> bool {
-    IMAGE_REG_EXP.is_match(name)
-}
-
-fn get_image(file: &mut ZipFile) -> Result<DynamicImage> {
-    let mut raw_image = Vec::new();
-    file.read_to_end(&mut raw_image)?;
-    let image = image::load_from_memory(&raw_image)?;
-
-    Ok(image)
 }
 // endregion
